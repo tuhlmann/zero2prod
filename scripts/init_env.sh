ec27f12fa90aec769eab7066a1188ff5b5a71136
Torsten Uhlmann <tuhlmann@agynamix.de>
2022-01-22 14:35:29 +0100

    Adding subscription route and infrastructure.

---
+++ scripts/init_env.sh
@@ -0,0 +1,6 @@
+DB_USER=${POSTGRES_USER:=tuhlmann}
+DB_PASSWORD="${POSTGRES_PASSWORD:=tuhlmann}"
+DB_NAME="${POSTGRES_DB:=newsletter}"
+DB_PORT="${POSTGRES_PORT:=5432}"
+
+export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+#![allow(dead_code)]
+
+pub mod configuration;
+pub mod routes;
+pub mod startup;
